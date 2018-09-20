78cbe699f11e53397e7039178b719814b0ca4a4d
janZhao <zhaojian3513@163.com>
2018-09-20 14:42:38 +0800

    冒泡算法提交~~~

---
+++ ZJDataStructure/ZJDataStructure/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  TestDataStructure
+//
+//  Created by ZJ on 2018/9/5.
+//  Copyright © 2018年 jydZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "ZJList.h"
+#import "SortUtil.h"
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJDataStructure/ZJDataStructure/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  TestDataStructure
+//
+//  Created by ZJ on 2018/9/5.
+//  Copyright © 2018年 jydZJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [self testSort];
+    
+    [self testLinkList];
+    
+}
+
+-(void)testSort{
+    [SortUtil BubbleSort];
+}
+
+
+-(void)testLinkList{
+
+    ZJList *list = [ZJList listWithData:0];
+    for (int i=1; i<10; i++) {
+        [list appendData:i];
+    }
+    [list printList];
+    printf("\n");
+    
+    [list reverseList];
+    [list printList];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
